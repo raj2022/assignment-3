6a5a96feaddaff7a0a01faa3a16275792ebe201a
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-09-02 00:41:27 +0530

    Create prob4.cpp

---
+++ prob4.cpp
@@ -0,0 +1,18 @@
+# include <iostream> 
+using namespace std;
+
+
+int main()
+{
+
+ int a;
+ cout<<"enter a number";
+ cin>>a;
+ if ( a>0)
+    cout<<"It is positive";
+ else if (a<0)
+  cout<<"It is negative";
+ else if (a==0)
+cout<<"it is zero";
+return 0;
+}
