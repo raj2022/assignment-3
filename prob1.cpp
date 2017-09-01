e718b456d8dbeeeb19a659840a750142a5e80916
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-09-02 00:08:44 +0530

    Create prob1.cpp

---
+++ prob1.cpp
@@ -0,0 +1,18 @@
+# include <iostream> 
+using namespace std;
+
+
+int main()
+{
+
+ int a,b ;
+ cout<<"enter a number";
+ cin>>a;
+ cout<<"enter another number";
+ cin>>b;
+ if ( a>b )
+    cout<<"greater number is"<<a ;
+ else 
+  cout<<"smaller  number is"<<b;
+return 0;
+}
