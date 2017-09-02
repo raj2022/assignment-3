ff7c14ff24df1cf818b967600d2baec1050a7921
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-09-02 15:15:11 +0530

    Create prob9.cpp

---
+++ prob9.cpp
@@ -0,0 +1,19 @@
+# include <iostream> 
+using namespace std;
+
+
+int main()
+{
+
+ char ch;
+ cout<<"enter the character";
+ cin>>ch;
+ if ((ch>= 'a'&& ch<= 'z')|| (ch>='A' && ch<='Z'))
+ cout<<ch<<" is an alphabet";
+ else 
+  if (ch>='0'&& ch<='9')
+ cout<<ch<<" is  a digit";
+ else
+cout<<ch<<"is a special character";
+return 0;
+}
