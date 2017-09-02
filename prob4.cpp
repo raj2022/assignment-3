f4a44e79610d62f1f7264c530fa46ca41c5144f0
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-09-02 14:18:10 +0530

    Create prob4.cpp

---
+++ prob4.cpp
@@ -0,0 +1,15 @@
+# include <iostream> 
+using namespace std;
+
+
+int main()
+{
+
+ int a;
+ cout<<"enter a number";
+ cin>>a;
+ if ( a%5==0 && a%11==0)
+    cout<<"It is divisible by 5 and 11";
+ else cout<<"it is not dividible by 5 and 11";
+return 0;
+}
