bad260c4ef619382c7f998c8bf81a468546326d8
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-09-02 14:20:24 +0530

    Create prob5.cpp

---
+++ prob5.cpp
@@ -0,0 +1,15 @@
+# include <iostream> 
+using namespace std;
+
+
+int main()
+{
+
+ int a;
+ cout<<"enter a number";
+ cin>>a;
+ if ( a%2==0)
+    cout<<"It is an even number";
+ else cout<<"it is an odd number";
+return 0;
+}
