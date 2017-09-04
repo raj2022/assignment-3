7acc5fd1ba6efb7d45113fe0790fbf1f2ec62395
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-09-04 15:20:45 +0530

    Create prob12.cpp

---
+++ prob12.cpp
@@ -0,0 +1,35 @@
+# include<iostream> 
+using namespace std;
+
+
+int main ()
+{
+  int  a=0;
+cout<<"enter the month no. in a year (1-12)";
+cin>>a;
+if(a==1)
+cout<<"\n \nThe month is of JANUARY and no of days is 31";
+else if (a==2)
+cout<<"\n \nThe month  is of  FEBRUARY and have 28||29 days";
+else if (a==3)
+cout<<"\n \nThe month is of MARCH and have 31 days";
+else if (a==4)
+cout<<"\n \nthe month is APRIL and have 30 days";
+else if (a==5) 
+cout<<"\n \nthe month  is of MAY and have 31 days";
+else if (a==6)
+cout<<"\n \nthe month is of JUNE have 30 days";
+else if (a==7)
+cout<<"\n \nthe month is of JULY having 31 days";
+else if(a=8)
+cout<<"\n \nthe month is of AUGUST having 31 days ";
+else if (a=9)
+cout<<"\n \nthe month is of SEPTEMBER having 30 days";
+else if (a=10)
+cout<<"\n \nthe month is of OCTOBER having 31 DAYS";
+else if (a=11)
+cout<<"\n \nthe month is of NOVEMBER having 30 days";
+else if (a=12)
+cout<<"\n \nthe month is of DECEMBER having 31 days";
+return 0;
+}
