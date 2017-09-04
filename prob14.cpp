3572af65f6c211a856e31b00d9360e3317644e01
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-09-04 15:38:53 +0530

    Create prob14.cpp

---
+++ prob14.cpp
@@ -0,0 +1,18 @@
+# include<iostream>
+using namespace std;
+
+/*Program to input angles of triangles and check whether triangle is valid or not.*/
+int main ()
+{
+float a,b,c,sum;
+cout<<"program to enter angles of triangles";
+cout<<"\n \nenter angles of triangle";
+cin>>a>>b>>c;
+sum =a+b+c;
+if 
+ (sum=180)
+cout<<"\n \n this triangle is can be formed with specified angles";
+else
+cout<<"\n \nwe can't have triangle with the specified angles.";
+return 0;
+}
