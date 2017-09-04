9a4bb767c7dc48d7d1969d18b1714ef745be1aad
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-09-04 14:46:50 +0530

    Create prob 10.cpp

---
+++ prob 10.cpp
@@ -0,0 +1,16 @@
+# include<iostream> 
+using namespace std;
+
+
+int main ()
+{
+  char ch;
+cout<<"enter the character";
+cin>>ch;
+if (ch >='a'&&ch<='z')
+cout <<ch<<"is of lowercase";
+else if (ch>='A'&&ch<='Z')
+cout <<ch<<"is of upperacse";
+return 0;
+}
+
