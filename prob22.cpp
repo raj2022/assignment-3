642c01b2ba0966d7267e83f5436dd500538e4452
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-09-05 15:53:32 +0530

    Create prob22.cpp

---
+++ prob22.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*Program to print numbers from 1 to n*/
+int main()
+
+{
+int n=0,n=1;
+cout<<"program to print number from 1 to n";
+cout<<"\n \nEnter the limit of the loop: ";
+  cin>>n; 
+  cout<<"\nThe numbers from 1 to "<<n<<" are: ";
+  while(i<=n)
+  {
+    cout<<"\n"<<i;
+    ++i;
+  }
+  return 0;
+}
